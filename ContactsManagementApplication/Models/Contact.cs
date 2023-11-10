672943641ef312101dc9eb4c67e3db87f95f4a75
remis-tic <142209912+remis-tic@users.noreply.github.com>
2023-11-10 21:59:35 +0530

    add model and data

---
+++ ContactsManagementApplication/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactsManagementApplication.Models;
+
+public class Contact
+{
+    [Required]
+    public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(255)]
+    public string FirstName { get; set; }
+    
+    [Required]
+    [MaxLength(255)]
+    public string LastName { get; set; }
+    
+    [Required]
+    [MaxLength(255)]
+    public string Email { get; set; }
+
+}
