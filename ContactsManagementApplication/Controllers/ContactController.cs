195ecd3afe34a67401322badc2ce928697de3b97
remis-tic <142209912+remis-tic@users.noreply.github.com>
2023-11-10 21:59:47 +0530

    setup controller

---
+++ ContactsManagementApplication/Controllers/ContactController.cs
@@ -0,0 +1,52 @@
+﻿using ContactsManagementApplication.Services;
+using Microsoft.AspNetCore.Mvc;
+using ContactsManagementApplication.Models;
+
+namespace ContactsManagementApplication.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class ContactController : ControllerBase
+{
+    private readonly IContactService contactService;
+    public ContactController(IContactService contactService)
+    {
+        this.contactService = contactService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await this.contactService.GetAllAsync());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        return Ok(await this.contactService.GetByIdAsync(id));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCount()
+    {
+        return Ok(await this.contactService.GetCountAsync());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Insert([FromBody] Contact contact)
+    {
+        return Ok(await this.contactService.InsertAsync(contact));
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update([FromBody] Contact contact)
+    {
+        return Ok(await this.contactService.UpdateAsync(contact.Id, contact));
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(int id)
+    {
+        return Ok(await this.contactService.DeleteAsync(id));
+    }
+}
