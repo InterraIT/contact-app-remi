66de61e8df1ed9e06fdb041c4714d9bc039d9b68
remis-tic <142209912+remis-tic@users.noreply.github.com>
2023-11-10 21:59:54 +0530

    add services

---
+++ ContactsManagementApplication/Services/ContactService.cs
@@ -0,0 +1,43 @@
+﻿using ContactsManagementApplication.Models;
+using ContactsManagementApplication.Repositories;
+
+namespace ContactsManagementApplication.Services;
+
+public class ContactService : IContactService
+{
+    private readonly IContactRepository contactRepository;
+    public ContactService(IContactRepository contactRepository)
+    {
+        this.contactRepository = contactRepository;
+    }
+
+    public async Task<int> DeleteAsync(int id)
+    {
+        return await contactRepository.DeleteAsync(id);
+    }
+
+    public async Task<IEnumerable<Contact>> GetAllAsync()
+    {
+        return await contactRepository.GetAllAsync();
+    }
+
+    public async Task<Contact> GetByIdAsync(int id)
+    {
+        return await contactRepository.GetByIdAsync(id);
+    }
+
+    public async Task<int> GetCountAsync()
+    {
+        return await contactRepository.GetCountAsync();
+    }
+
+    public async Task<Contact> InsertAsync(Contact contact)
+    {
+        return await contactRepository.InsertAsync(contact);
+    }
+
+    public async Task<int> UpdateAsync(int id, Contact contact)
+    {
+        return await contactRepository.UpdateAsync(id, contact);
+    }
+}
---
+++ ContactsManagementApplication/Services/IContactService.cs
@@ -0,0 +1,14 @@
+﻿using ContactsManagementApplication.Models;
+using System.Diagnostics.Metrics;
+
+namespace ContactsManagementApplication.Services;
+
+public interface IContactService
+{
+    Task<int> DeleteAsync(int id);
+    Task<IEnumerable<Contact>> GetAllAsync();
+    Task<Contact> GetByIdAsync(int id);
+    Task<int> GetCountAsync();
+    Task<Contact> InsertAsync(Contact contact);
+    Task<int> UpdateAsync(int id, Contact contact);
+}
